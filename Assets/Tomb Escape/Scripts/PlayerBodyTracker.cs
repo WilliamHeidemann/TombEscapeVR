73fdc229f440c183c29037a31e2a05366d3f6656
William Skou Heidemann <n11511591@qut.edu.au>
2023-04-06 14:14:19 +1000

    update

--- Assets/Tomb Escape/Scripts/PlayerBodyTracker.cs
+++ Assets/Tomb Escape/Scripts/PlayerBodyTracker.cs
@@ -13,6 +13,8 @@ public class PlayerBodyTracker : MonoBehaviour
     [SerializeField] private MeshRenderer solvedPositionOneIndicator;
     [SerializeField] private MeshRenderer solvedPositionTwoIndicator;
     [SerializeField] private MeshRenderer solvedPositionThreeIndicator;
+
+    [SerializeField] private Transform player;
     
     private bool _isTracking;
     private bool _firstPositionDone;
@@ -20,17 +22,26 @@ public class PlayerBodyTracker : MonoBehaviour
     private bool _thirdPositionDone;
 
     private float _distanceBetweenHands;
+    private Vector3 _distanceVector;
     private void Awake()
     {
         Fader.MoveRoom += (room) => _isTracking = room == roomToTrack; //<>
     }
-
+    
     private void Update()
     {
         if (!_isTracking) return;
         _distanceBetweenHands = Vector3.Distance(rightHand.position, leftHand.position);
         if (_distanceBetweenHands == 0) return; // One controller not detected
-        if (_distanceBetweenHands < 8) return; // Hands too close
+        _distanceVector = rightHand.position - leftHand.position;
+        // var rotation = Quaternion.FromToRotation(Vector3.left, _distanceVector.normalized);
+        // var rotatedDistanceVector = rotation * _distanceVector ;
+        // var handsVector = rotatedDistanceVector.normalized * _distanceBetweenHands;
+        var projection = Vector3.Project(_distanceVector, player.forward);
+        var perpendicular = _distanceVector - projection;
+        var aligned = Quaternion.FromToRotation(Vector3.forward, player.forward) * projection;
+        var handsVector = aligned + perpendicular;
+        print("æ " + handsVector);
         if (IsPoseOne())
         {
             _firstPositionDone = true;
@@ -56,21 +67,23 @@ public class PlayerBodyTracker : MonoBehaviour
 
     private bool IsPoseOne()
     {
-        var distanceVector =  rightHand.position - leftHand.position;
-        if (distanceVector.x < 4) return false;
-        if (distanceVector.y < 4) return false;
+        if (_distanceVector.x < 4) return false;
+        if (_distanceVector.y < 4) return false;
+        if (Mathf.Abs(_distanceVector.z) > 4) return false;
         return true;
     }
     private bool IsPoseTwo()
     {
-        var angleDifference = Quaternion.Angle(rightHand.rotation, leftHand.rotation);
-        return angleDifference > 170f;
+        if (_distanceVector.x < 6) return false;
+        if (Mathf.Abs(_distanceVector.y) > 4) return false;
+        if (Mathf.Abs(_distanceVector.z) > 4) return false;
+        return true;
     }
     private bool IsPoseThree()
     {
-        var distanceVector =  rightHand.position - leftHand.position;
-        if (distanceVector.x > -4) return false;
-        if (distanceVector.z < 4) return false;
+        if (_distanceVector.x < 4) return false;
+        if (_distanceVector.y > -4) return false;
+        if (_distanceVector.z < 4) return false;
         return true;
     }
 }
